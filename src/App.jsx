e27138e8a7ebf5c1dfd3e8dd17073d93400358cc
Aryan Thakur <aryanthakur122003@gmail.com>
2025-04-02 02:21:24 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { Routes, Route,} from "react-router-dom";
-import Home from "./Pages/Home.jsx";
+import { Routes, Route } from "react-router-dom";
 import MovieInfo from "./pages/MovieInfo.jsx";
+import Home from "./pages/Home.jsx"; 
 
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Home/>} />
-      < Route path="/movie/:id" element={<MovieInfo/>}/>
-      </Routes>
- 
-  )
-  
+      <Route path="/" element={<Home/>} />
+      <Route path="/movie/:id" element={<MovieInfo/>} />
+    </Routes>
+  );
 }
 
 export default App;
