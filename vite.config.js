6694b5fc71040515a3e567a8f0306191aae0e322
Aryan Thakur <aryanthakur122003@gmail.com>
2025-04-02 02:55:37 +0530

    one more

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
-  server: {
-    fs: {
-      strict: false // Allows case-insensitive imports
-    }
-  }
+  
 })
