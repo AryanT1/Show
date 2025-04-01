bed2a112726c7e9811f70ae6d45db3d3a54fd592
Aryan Thakur <aryanthakur122003@gmail.com>
2025-04-02 01:31:24 +0530

    changed name of a file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route,} from "react-router-dom";
 import Home from "./Pages/Home.jsx";
-import MoiveInfo from "./pages/MovieInfo.jsx";
+import MovieInfo from "./pages/MovieInfo.jsx";
 
 function App() {
   return (
     <Routes>
         <Route path="/" element={<Home/>} />
-      < Route path="/movie/:id" element={<MoiveInfo/>}/>
+      < Route path="/movie/:id" element={<MovieInfo/>}/>
       </Routes>
  
   )
